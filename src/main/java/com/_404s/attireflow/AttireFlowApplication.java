9a1f05679d67ede70a2d41322b7d48978ab014b6
Joseph Nahas <joe_nahas@hotmail.com>
2026-02-12 17:10:28 -0500

    Initial commit

---
+++ src/main/java/com/_404s/attireflow/AttireFlowApplication.java
@@ -0,0 +1,13 @@
+package com._404s.attireflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttireFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttireFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_404s/attireflow/AttireFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com._404s.attireflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttireFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
